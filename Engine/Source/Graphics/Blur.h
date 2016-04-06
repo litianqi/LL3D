c46e424044b6cae0352422bf1047906f5ac9da2a
Louis Li <me@louis.li>
2016-04-06 16:28:16 +0800

    Add blur

---
+++ Engine/Source/Graphics/Blur.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class BlurEffect {
+public:
+  BlurEffect();
+
+private:
+
+};
+
+//-----------------------------------------------------------------------------
+// A utility to blur a givn image or 
+class Blur {
+
+};
