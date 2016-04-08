0aa73bdf7287971ee415eaafaa3b71898c4c28d4
Louis Li <me@louis.li>
2016-04-08 14:51:36 +0800

    DirectXTK: Move DirectXTK into Engine

--- Test/TestCamera.cpp
+++ Test/TestCamera.cpp
@@ -1,7 +1,13 @@
-#include "stdafx.h"
-#include "CppUnitTest.h"
+#include <stdafx.h>
+#include <CppUnitTest.h>
+#include <Graphics/Camera.h>
+#include <Math/Math.h>
+#include <GameObject.h>
+#include <Transform.h>
 
+using namespace std;
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace LL3D;
 
 namespace Test
 {		
@@ -9,10 +15,29 @@ namespace Test
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestDirection)
 		{
-			// TODO: Your test code here
+      auto object = ConstructAGameObject();
+      Assert::AreEqual(1, 1);
 		}
 
+  private:
+    static GameObject ConstructAGameObject() 
+    {
+      auto camera = make_unique<Graphics::Camera>(Graphics::Camera::Frustum(
+        Math::XM_PI / 8.f,
+        0.5f,
+        1,
+        1000),
+        Math::Vector3(0.f, -100.f, 100.f)
+        );
+
+      auto object =GameObject();
+      
+      object.GetTransform().SetPosition(Math::Vector3(0.f, 100.f, -100.f));
+      object.AddComponent(std::move(camera));
+
+      return object;
+    }
 	};
 }
